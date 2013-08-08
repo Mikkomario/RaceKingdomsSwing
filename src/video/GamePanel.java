47b189a913b0346c789e5be31357b8e01df3a8dc
Unto <unto.solala@aalto.fi>
2013-08-08 16:23:48 +0300

    Made two new classes: GameWindow and GamePanel

---
+++ src/video/GamePanel.java
@@ -0,0 +1,89 @@
+package video;
+
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+
+/**
+ * Creates a panel for the game.
+ * 
+ * @author Unto Created 8.8.2013
+ * 
+ */
+public class GamePanel extends JPanel{
+	
+	// ATTRIBUTES ---------------------------------------------------------
+	
+	private int width;
+	private int height;
+	
+	// CONSTRUCTOR ---------------------------------------------------------
+	/**Creates a new panel with default color being
+	 * 
+	 * @param width		Panel's width
+	 * @param height	Panel's height
+	 */
+	public GamePanel(int width, int height){
+		this.width = width;
+		this.height = height;
+		//Let's format our panel
+		this.formatPanel();
+		//And make it visible
+		this.setVisible(true);
+		System.out.println("Panel's width: "+this.width+" Panel's height: "+this.height);
+	}
+	
+	// PRIVATE METHODS ---------------------------------------------------
+	
+	private void formatPanel(){
+		//Let's set the panel's size...
+		this.setSizes(this.width, this.height);
+		//...And color
+		this.setBackground(new Color(0,0,0,255));
+	}
+	
+	// METHODS ---------------------------------------------------
+	
+	/**Changes the size of the game panel, for real.
+	 * 
+	 * @param width		Panel's new width
+	 * @param height	Panel's new height
+	 */
+	public void setSizes(int width, int height){
+		this.setSize(width, height);
+		Dimension preferred = new Dimension(width, height);
+		this.setPreferredSize(preferred);
+		this.setMinimumSize(preferred);
+		this.setMaximumSize(preferred);
+	}
+	
+
+	
+	/**Changes the panel's background color.
+	 * 
+	 * @param red
+	 * @param green
+	 * @param blue
+	 * @param alpha
+	 */
+	public void setBackgroundColor(int red, int green, int blue, int alpha){
+		this.setBackground(new Color(red, green, blue, alpha));
+	}
+	
+	/**Makes the panel visible.
+	 * 
+	 */
+	public void makeVisible(){
+		this.setVisible(true);
+	}
+	
+	/**Makes the panel invisible.
+	 * 
+	 */
+	public void makeInvisible(){
+		this.setVisible(false);
+	}
+	
+
+}
---
+++ src/video/GameWindow.java
@@ -0,0 +1,84 @@
+package video;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+
+/**
+ * Creates the main window frame.
+ * 
+ * @author Unto Created 8.8.2013
+ * 
+ */
+public class GameWindow extends JFrame{
+	
+	// ATTRIBUTES ---------------------------------------------------------
+	
+	private int width;
+	private int height;
+	
+	// CONSTRUCTOR ---------------------------------------------------------
+	
+	/**Creates a new window frame with given width and height.
+	 * 
+	 * @param width		Window's width.
+	 * @param height	Window's height.
+	 */
+	public GameWindow(int width, int height){
+		this.width = width;
+		this.height = height;
+		//Let's format our window
+		this.formatWindow();
+		//And make it visible
+		this.setVisible(true);
+	}
+	
+	/**Creates a new window frame with given width, height and name.
+	 * 
+	 * @param width		Window's width.
+	 * @param height	Window's height.
+	 * @param WindowName	Frame's name.
+	 */
+	public GameWindow(int width, int height, String WindowName){
+		this.width = width;
+		this.height = height;
+		//Let's format our window
+		this.formatWindow();
+		//Let's set our window's title
+		this.setTitle(WindowName);
+		//And make it visible
+		this.setVisible(true);
+	}
+	
+	// PRIVATE METHODS ---------------------------------------------------
+	
+	private void formatWindow(){
+		//Let's set our window's layout
+		this.setLayout(new BorderLayout());
+		//Let's make sure our window closes properly
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//Let's set our window's size
+		this.setSize(this.width, this.height);
+	}
+	
+	
+	// METHODS ---------------------------------------------------
+	
+	public void addGamePanel(GamePanel newPanel, String direction){
+		this.add(newPanel, direction);
+	}
+	
+	
+	
+	// MAIN METHOD ---------------------------------------------------
+	
+	public static void main(String [] args){
+		GameWindow testi = new GameWindow(800, 600, "Testi");
+		GamePanel paneeliTesti = new GamePanel(100, 100);
+		GamePanel toinenPaneeli = new GamePanel(200, 200);
+		toinenPaneeli.setBackgroundColor(123, 123, 0, 255);
+		testi.addGamePanel(paneeliTesti, BorderLayout.WEST);
+		testi.addGamePanel(toinenPaneeli, BorderLayout.EAST);
+	}
+
+}
